62bbf466186eae807875abf7a2ba079fc2f95377
Yaroslav Kozlov <kozlov.ya.tochka@gmail.com>
2019-04-22 17:57:46 +0500

    Создание исполняемого файла

---
+++ src/Exchange.java
@@ -0,0 +1,21 @@
+public class Exchange {
+    public static void main (String[] args) {
+
+        System.out.println("Введите валюту которую хотите обменять:");
+        // RUB
+        System.out.println("Введите сумму:");
+        // 100
+        System.out.println("Введите валюту для обмена:");
+        // USD
+        System.out.println("Немного терпения...");
+        System.out.println("Вы действительно хотите купить 1.50 $ за 100 \u20BD ?");
+        System.out.println("Введите \"y\" если согласны, \"n\" для отмены.");
+        // Ожидание ввода
+        System.out.println("Вы совершили обмен: Купили 1.50 $ за 100 \u20BD.");
+        System.out.println("Хотите обменять еще?");
+        System.out.println("Введите \"y\" если согласны, \"n\" для отмены.");
+
+        // Ожидание ввода
+        System.out.println("$ \u20BD");
+    }
+}
