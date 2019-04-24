50d4b88f1ea91d8cc7934c1c0067759a5b17a7a6
YAroslav Kozlov <kozlov.ya.tochka@gmail.com>
2019-04-24 21:00:24 +0500

    рефакторинг исполняемого файла

--- src/Exchange.java
+++ src/Exchange.java
@@ -11,24 +11,17 @@ public class Exchange {
 
         do {
             System.out.println("Введите валюту которую хотите обменять:");
-            currencyConvertFrom = scanner.next();
-            // RUB
+            currencyConvertFrom = scanner.next(); // вводим валюту которую хотим обменять
             System.out.println("Введите сумму:");
-            amountConvertFrom = scanner.nextBigDecimal();
-            // 100
+            amountConvertFrom = scanner.nextBigDecimal(); // вводим сумму которую хотим обменять
             System.out.println("Введите валюту для обмена:");
-            currencyConvertTo = scanner.next();
-            // USD
+            currencyConvertTo = scanner.next(); // вводим
             System.out.println("Немного терпения...");
             Thread.sleep(2000);
             System.out.println("Вы действительно хотите купить 1.50 $ за " + amountConvertFrom + " \u20BD ?");
             System.out.println("Введите \"y\" если согласны, \"n\" для отмены.");
-            confirmConvert = scanner.next();
-            // Ожидание ввода
+            confirmConvert = scanner.next(); // ввод подтверждения
             System.out.println("Вы совершили обмен: Купили 1.50 $ за " + amountConvertFrom + " \u20BD.");
-            System.out.println("€\n" +
-                    "¥\n" +
-                    "$");
             System.out.println("Хотите обменять еще?");
             System.out.println("Введите \"y\" если согласны, \"n\" для отмены.");
             confirmRepeat = scanner.next();
@@ -45,3 +38,7 @@ public class Exchange {
 
     }
 }
+
+//System.out.println("€\n" +
+//        "¥\n" +
+//        "$");
